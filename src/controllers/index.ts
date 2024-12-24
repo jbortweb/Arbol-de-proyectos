40d73280b8358fe0cdae3ead902eb511a88bd233
Jbortweb <jbortweb@gmail.com>
2024-12-24 09:36:46 +0100

    hash password and validation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express'
+import { validationResult } from 'express-validator'
+import slug from 'slug'
 import User from '../models/User'
+import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-  const { email } = req.body
+  let errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() })
+    return
+  }
+  const { email, password } = req.body
+
   const userExists = await User.findOne({ email })
   if (userExists) {
     const error = new Error('El email ya está en uso')
@@ -10,7 +19,17 @@ export const createAccount = async (req: Request, res: Response) => {
     return
   }
 
+  const handle = slug(req.body.handle, '')
+  const handleExists = await User.findOne({ handle })
+  if (handleExists) {
+    const error = new Error('El nombre de usuario no esta disponible')
+    res.status(409).json({ error: error.message })
+    return
+  }
+
   const user = new User(req.body)
+  user.password = await hashPassword(password)
+  user.handle = handle
   await user.save()
 
   res.status(201).send('Registro creado correctamente')
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createAccount } from './controllers'
 
 const router = Router()
 
-router.post('/auth/register', createAccount)
+router.post(
+  '/auth/register',
+  body('handle')
+    .notEmpty()
+    .withMessage('El nombre de usuario no puede ir vacío'),
+  body('name').notEmpty().withMessage('El nombre no puede ir vacío'),
+  body('email').isEmail().withMessage('El email no es correcto'),
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('La contraseña debe tener al menos 8 caracteres'),
+  createAccount
+)
 
 export default router
