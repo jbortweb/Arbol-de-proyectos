628fee53fc98749866028dfec11b2e098fd032ce
Jbortweb <jbortweb@gmail.com>
2024-12-25 12:29:37 +0100

    Auth users

--- frontend/src/types/index.tsx
+++ frontend/src/types/index.tsx
@@ -8,3 +8,4 @@ export type RegisterForm = Pick<User, 'handle' | 'name' | 'email'> & {
   password: string
   password_confirmation: string
 }
+export type LoginForm = Pick<User, 'email'> & { password: string }
--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,66 +1,85 @@
 import { Link } from 'react-router-dom'
+import { LoginForm } from '../types'
+import { useForm } from 'react-hook-form'
+import ErrorMessage from '../components/ErrorMessage'
+import api from '../config/axios'
+import { toast } from 'sonner'
+import { isAxiosError } from 'axios'
 
 export default function Login() {
+  const initialValues: LoginForm = {
+    email: '',
+    password: '',
+  }
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues })
+
+  const handleLogin = async (formData: LoginForm) => {
+    try {
+      const { data } = await api.post(`/auth/login`, formData)
+      toast.success(data)
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        toast.error(error.response.data.error)
+      }
+    }
+  }
+
   return (
     <>
       <h1 className="text-4xl font-black text-white">Inicia Sesión</h1>
-      {/*  <form 
-    onSubmit={() => {}}
-    className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
->
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
-        <input
-            id="name"
-            type="text"
-            placeholder="Tu Nombre"
-            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-        />
-    </div>
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
-        <input
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+        noValidate
+      >
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="email" className="text-2xl text-slate-500">
+            E-mail
+          </label>
+          <input
             id="email"
             type="email"
             placeholder="Email de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-        />
-    </div>
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
-        <input
-            id="handle"
-            type="text"
-            placeholder="Nombre de usuario: sin espacios"
-            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-        />
-    </div>
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
-        <input
+            {...register('email', {
+              required: 'El Email es obligatorio',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: 'E-mail no válido',
+              },
+            })}
+          />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password" className="text-2xl text-slate-500">
+            Password
+          </label>
+          <input
             id="password"
             type="password"
             placeholder="Password de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-        />
-    </div>
+            {...register('password', {
+              required: 'El Password es obligatorio',
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+        </div>
 
-    <div className="grid grid-cols-1 space-y-3">
-        <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
         <input
-            id="password"
-            type="password"
-            placeholder="Repetir Password"
-            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+          type="submit"
+          className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+          value="Iniciar Sesión"
         />
-    </div>
-
-    <input
-        type="submit"
-        className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
-        value='Crear Cuenta'
-    />  
-</form> */}
+      </form>
       <nav className="mt-6">
         <Link
           className="text-lg text-center text-white block"
