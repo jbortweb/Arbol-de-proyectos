107baf87a498f48f7570fd2360327bdb8efbf691
Jbortweb <jbortweb@gmail.com>
2024-12-26 08:43:12 +0100

    Obtained user information and displayed in the profile

--- frontend/src/api/ArbolAPI.ts
+++ frontend/src/api/ArbolAPI.ts
@@ -1,9 +1,10 @@
 import { isAxiosError } from 'axios'
 import api from '../config/axios'
+import { User } from '../types'
 
 export async function getUser() {
   try {
-    const { data } = await api.get('user')
+    const { data } = await api.get<User>('user')
     return data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
